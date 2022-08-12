b2a3938941ab5989c0231d9fc81ae63a4959479f
Gaurav verma <gauravofficialverma@gmail.com>
2022-08-11 23:05:34 -0400

    change brand name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/cibc'),
+      dir: require('path').join(__dirname, './coverage/banking'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './gaurds/auth.guard'
 
 /** Homepage is /banks
